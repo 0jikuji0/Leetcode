cf13903d935886ae7bf77031a518ec9ff053ca13
jikuji <jikuji@2a01cb0811e711006dec0f2c99b7a9ee.ipv6.abo.wanadoo.fr>
2026-01-14 18:10:55 +0100

    ContainerWithMostWater in C

---
+++ ContainerWithMostWater/ContainerWithMostWater.c
@@ -0,0 +1,25 @@
+int maxArea(int* height, int heightSize) {
+    int left = 0;
+    int right = heightSize - 1;
+    int maxWater = 0;
+
+    while (left < right){
+        int width = right - left;
+
+        int h = (height[left] < height[right]) ? height[left] : height[right];
+
+        int currentArea = width *h;
+
+        if (currentArea > maxWater){
+            maxWater = currentArea;
+        }
+
+        if (height[left] < height[right]){
+            ++left;
+        } else {
+            --right;
+        }
+    }
+
+    return maxWater;
+}
