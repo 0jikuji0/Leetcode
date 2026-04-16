ae87982f24bae432a17bdb0d605a87e588c58e12
jikuji/ <the.jikuji_dev@proton.me>
2026-04-16 12:03:46 +0200

    Remove unused index variable in IntegertoRoman
    Removed unnecessary index variable from the function.

--- 12.IntegertoRoman/IntegertoRoman.c
+++ 12.IntegertoRoman/IntegertoRoman.c
@@ -6,8 +6,6 @@ char* intToRoman(int num) {
     int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     char* symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-    int index = 0;
-
     for (int i = 0; i < 13; ++i){
         while(num >= values[i]){
             strcat(result, symbols[i]);
